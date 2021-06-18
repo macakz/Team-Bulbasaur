58125214ffb001e83bbc23c916a10d9d2126b0f1
Alexander McKerrow <mckerrow4@gmail.com>
2021-06-18 16:15:28 +1200

    fixed header image

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import colors from '../colors'
+import pokemons from '../../data/pokemon'
 
 function Header () {
   return (
     <>
       <div className="flex justify-center container mx-auto">
         <div className="flex p-4 items-center">
-          <img src="https://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png" className="h-8 w-8" />
+          <img src={"/images/pokemon/" + "001.png"} className="h-8 w-8" />
           <div style={{ color: colors.color }}>TEAM  BULBASAUR</div>
         </div>
       </div>
